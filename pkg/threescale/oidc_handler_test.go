f40c87562e6034504d117fc9c6a0d12b60d3f263
Philip Gough <philip.p.gough@gmail.com>
2019-04-11 12:50:05 +0100

    test: adapter: oidc Adds test for provider creation
    Change adds a unit test for providing an 'OIDC Handler' with a given url which will be used via
    the discovery protocol in OIDC flow.
    
    Test adds a number of helpers which includes the ability to generate some valid JWK
    using the 'github.com/lestrrat-go/jwx/jwk' package.
    
    Introduces a mock test server which simulates a remote keycloak and respinds with
    the certs (JWK) and a valid discovery response.

---
+++ pkg/threescale/oidc_handler_test.go
@@ -0,0 +1,178 @@
+package threescale
+
+import (
+	"crypto/rand"
+	"crypto/rsa"
+	"encoding/json"
+	"fmt"
+	"net"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/lestrrat-go/jwx/jwk"
+)
+
+var globalPK *rsa.PrivateKey
+
+func init() {
+	pk, err := rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		panic(fmt.Errorf("failed to generate private key: %s", err))
+	}
+	globalPK = pk
+}
+
+func TestOIDCHandler_CreateProvider(t *testing.T) {
+	const listener = "127.0.0.1:8090"
+	const issuer = "http://" + listener
+
+	ts := createTestServer(listener, t)
+	o := NewOIDCHandler(nil)
+
+	// create provider aainst the test server and expect it to work without error and provider not be nil
+	p, err := o.CreateProvider(issuer)
+	if err != nil || p == nil {
+		t.Errorf("unexpected error when creating provider - %v", err)
+	}
+	ts.Close()
+
+	//retry after closing the server
+	o = NewOIDCHandler(&http.Client{
+		Timeout: time.Millisecond,
+	})
+	p, err = o.CreateProvider(issuer)
+	if err == nil || p != nil {
+		t.Errorf("expected error when creating provider - %v", err)
+	}
+
+	//create with invalid url
+	_, err = o.CreateProvider("~invalid")
+	if err == nil {
+		t.Errorf("expected error when creating provide with invalid url - %v", err)
+	}
+
+}
+
+func getDiscoveryResponse(providerUrl string) []byte {
+	return []byte(fmt.Sprintf(`{  
+   "issuer":"%s",
+   "authorization_endpoint":"%s/auth/realms/3scale-keycloak/protocol/openid-connect/auth",
+   "token_endpoint":"%s/auth/realms/3scale-keycloak/protocol/openid-connect/token",
+   "userinfo_endpoint":"%s/auth/realms/3scale-keycloak/protocol/openid-connect/userinfo",
+   "jwks_uri":"%s/auth/realms/3scale-keycloak/protocol/openid-connect/certs",
+   "grant_types_supported":[  
+      "authorization_code",
+      "implicit",
+      "refresh_token",
+      "password",
+      "client_credentials"
+   ],
+   "response_types_supported":[  
+      "code",
+      "none",
+      "id_token",
+      "token",
+      "id_token token",
+      "code id_token",
+      "code token",
+      "code id_token token"
+   ],
+   "subject_types_supported":[  
+      "public",
+      "pairwise"
+   ],
+   "id_token_signing_alg_values_supported":[  
+      "ES384",
+      "RS384",
+      "HS256",
+      "HS512",
+      "ES256",
+      "RS256",
+      "HS384",
+      "ES512",
+      "RS512"
+   ],
+   "userinfo_signing_alg_values_supported":[  
+      "ES384",
+      "RS384",
+      "HS256",
+      "HS512",
+      "ES256",
+      "RS256",
+      "HS384",
+      "ES512",
+      "RS512",
+      "none"
+   ],
+   "request_object_signing_alg_values_supported":["none","RS256"],
+   "response_modes_supported":["query"],
+   "registration_endpoint":"http://keycloak-keycloak.34.242.107.254.nip.io/auth/realms/3scale-keycloak/clients-registrations/openid-connect",
+   "token_endpoint_auth_methods_supported":[  
+      "private_key_jwt",
+      "client_secret_basic",
+      "client_secret_post",
+      "client_secret_jwt"
+   ],
+   "token_endpoint_auth_signing_alg_values_supported":["RS256","HS256"],
+   "claims_supported":[  
+      "sub",
+      "iss",
+      "name"
+   ],
+   "claim_types_supported":["normal"],
+   "claims_parameter_supported":false,
+   "scopes_supported":["openid"],
+   "request_parameter_supported":true,
+   "request_uri_parameter_supported":true,
+   "code_challenge_methods_supported":["plain","S256"]
+}`, providerUrl, providerUrl, providerUrl, providerUrl, providerUrl))
+}
+
+func buildJwk(t *testing.T) []byte {
+	t.Helper()
+
+	key, err := jwk.New(&globalPK.PublicKey)
+	if err != nil {
+		t.Fatalf("failed to create JWK: %s", err)
+	}
+
+	key.Set(jwk.KeyUsageKey, "enc")
+	key.Set(jwk.KeyIDKey, "enc1")
+
+	b, err := json.MarshalIndent(key, "", "  ")
+	if err != nil {
+		t.Fatalf("failed to generate JSON: %s", err)
+	}
+
+	str := fmt.Sprintf(`{"keys":[%s]}`, string(b))
+	b = []byte(str)
+
+	return b
+}
+
+// creates and starts a test server which listens on provided ip:port
+// this acts as a fake OIDC provider. This starts the server and the caller is responsible for closing
+// the connection when done.
+func createTestServer(listenOn string, t *testing.T) *httptest.Server {
+	t.Helper()
+	issuer := fmt.Sprintf("http://%s", listenOn)
+	l, err := net.Listen("tcp", listenOn)
+	if err != nil {
+		t.Fatalf("error listening on port for test data")
+	}
+
+	ts := httptest.NewUnstartedServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		if strings.Contains(r.URL.Path, "auth/realms/3scale-keycloak/protocol/openid-connect/certs") {
+			w.Write(buildJwk(t))
+		} else {
+			w.Write(getDiscoveryResponse(issuer))
+		}
+	}))
+	ts.Listener = l
+	ts.Start()
+	return ts
+}
