73a665674cbeda1d57f064a9daf3032baec4da2f
Philip Gough <philip.p.gough@gmail.com>
2020-01-13 16:06:09 +0000

    backend: Add authorize API to cached backend
    This changes satisfies the Authorize API for 3scale client.
    
    Authorizations are made against the linked cache if the key is
    available. If we fail to lookup the key and have a cache miss then we
    attempt to call out to the remote 3scale to get a known state and
    populate the cache with the application before making a grant/deny on
    the authoriztion request.

--- pkg/threescale/backend/backend.go
+++ pkg/threescale/backend/backend.go
@@ -1,6 +1,7 @@
 package backend
 
 import (
+	"fmt"
 	"sync"
 
 	"github.com/3scale/3scale-go-client/threescale"
@@ -36,6 +37,101 @@ type LimitCounter map[string]map[api.Period]*Limit
 
 // UnlimitedCounter keeps a count of metrics without limits
 type UnlimitedCounter map[string]int
+
+// Authorize authorizes a request based on the current cached values
+// If the request misses the cache, a remote call to 3scale is made
+// Request Transactions must not be nil and must not be empty
+// If multiple transactions are provided, all but the first is discarded
+func (b *Backend) Authorize(request threescale.Request) (*threescale.AuthorizeResult, error) {
+	return b.authorize(request)
+}
+
+func (b *Backend) authorize(request threescale.Request) (*threescale.AuthorizeResult, error) {
+	var err error
+
+	err = validateTransactions(request.Transactions)
+	if err != nil {
+		return nil, err
+	}
+
+	cacheKey := generateCacheKeyFromRequest(request)
+	app := b.getApplicationFromCache(cacheKey)
+
+	if app == nil {
+		app, err = b.handleCacheMiss(request, cacheKey)
+		if err != nil {
+			return nil, fmt.Errorf("unable to process request - %s", err.Error())
+		}
+	}
+
+	affectedMetrics := computeAffectedMetrics(app, request)
+	isAuthorized := b.isAuthorized(app, affectedMetrics)
+
+	result := &threescale.AuthorizeResult{Authorized: isAuthorized}
+	if !isAuthorized {
+		result.ErrorCode = "usage limits are exceeded"
+	}
+
+	return result, nil
+}
+
+// handleCacheMiss attempts to call remote 3scale with a blank request and the
+// required extensions enabled to learn current state for caching purposes.
+// Sets the value in the cache and returns the newly cached value for re-use if required
+func (b *Backend) handleCacheMiss(request threescale.Request, cacheKey string) (*Application, error) {
+	var app Application
+
+	emptyTransaction := emptyTransactionFrom(request.Transactions[0])
+	emptyRequest := getEmptyAuthRequest(request.Service, request.Auth, emptyTransaction.Params)
+
+	resp, err := b.remoteAuth(emptyRequest)
+	if err != nil {
+		return nil, err
+	}
+	app = getApplicationFromResponse(resp)
+	app.auth = request.Auth
+	app.params = request.Transactions[0].Params
+	app.metricHierarchy = resp.Hierarchy
+
+	b.cache.Set(cacheKey, &app)
+	return &app, nil
+}
+
+// isAuthorized takes a read lock on the application and confirms if the request
+// should be authorized based on the affected metrics against current state
+func (b *Backend) isAuthorized(application *Application, affectedMetrics api.Metrics) bool {
+	application.RLock()
+	defer application.RUnlock()
+
+	authorized := true
+
+out:
+	for metric, incrementBy := range affectedMetrics {
+		cachedValue, ok := application.LimitCounter[metric]
+		if ok {
+			for _, limit := range cachedValue {
+				if limit.current+incrementBy > limit.max {
+					authorized = false
+					break out
+				}
+			}
+		}
+	}
+	return authorized
+}
+
+func (b *Backend) getApplicationFromCache(key string) *Application {
+	app, ok := b.cache.Get(key)
+	if !ok {
+		app = nil
+	}
+	return app
+}
+
+// remoteAuth calls Authorize API of the underlying client
+func (b *Backend) remoteAuth(request threescale.Request) (*threescale.AuthorizeResult, error) {
+	return b.client.Authorize(request)
+}
 
 // deepCopy creates a clone of the LimitCounter 'lc'
 func (lc LimitCounter) deepCopy() LimitCounter {
