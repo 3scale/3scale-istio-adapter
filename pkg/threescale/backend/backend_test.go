4fc8d1b331c9e6bc540780d6be68cc7a063f11aa
Philip Gough <philip.p.gough@gmail.com>
2020-01-23 16:34:24 +0000

    test:backend: Add unit test for AuthRep function

--- pkg/threescale/backend/backend_test.go
+++ pkg/threescale/backend/backend_test.go
@@ -275,6 +275,388 @@ func TestBackend_Authorize(t *testing.T) {
 	}
 }
 
+func TestBackend_AuthRep(t *testing.T) {
+	const cacheKey = "test_application"
+
+	inputs := []struct {
+		name             string
+		setup            func(cacheable Cacheable, remoteClient *mockRemoteClient) *Backend
+		request          threescale.Request
+		expectError      bool
+		expectResult     *threescale.AuthorizeResult
+		expectCacheState Application
+	}{
+		{
+			name: "Test error when no transactions are provided",
+			setup: func(cacheable Cacheable, remoteClient *mockRemoteClient) *Backend {
+				return &Backend{
+					client: remoteClient,
+					cache:  cacheable,
+				}
+			},
+			request: threescale.Request{
+				Auth: api.ClientAuth{
+					Type:  api.ProviderKey,
+					Value: "any",
+				},
+				Service: "test",
+			},
+			expectError: true,
+		},
+		{
+			name: "Test cache miss and error from 3scale, can't fetch hierarchy returns error",
+			setup: func(cacheable Cacheable, remoteClient *mockRemoteClient) *Backend {
+				remoteClient.authErr(t, fmt.Errorf("some arbitary error"))
+				return &Backend{
+					client: remoteClient,
+					cache:  cacheable,
+				}
+			},
+			request: threescale.Request{
+				Auth: api.ClientAuth{
+					Type:  api.ProviderKey,
+					Value: "any",
+				},
+				Service: "test",
+				Transactions: []api.Transaction{
+					{
+						Metrics: api.Metrics{},
+						Params: api.Params{
+							AppID: "application",
+						},
+					},
+				},
+			},
+			expectError: true,
+		},
+		{
+			name: "Test failure on metrics with no parents that breach the limits - cache miss",
+			setup: func(cacheable Cacheable, remoteClient *mockRemoteClient) *Backend {
+				// we want to ensure the conversion is done here and the cache is populated correctly
+				remoteResponse := &threescale.AuthorizeResult{
+					Authorized: true,
+					AuthorizeExtensions: threescale.AuthorizeExtensions{
+						Hierarchy:  make(api.Hierarchy),
+						RateLimits: nil,
+						UsageReports: api.UsageReports{
+							"hits": api.UsageReport{
+								PeriodWindow: api.PeriodWindow{
+									Period: api.Minute,
+								},
+								MaxValue:     4,
+								CurrentValue: 3,
+							},
+						},
+					},
+				}
+
+				remoteClient.setAuthResponse(t, remoteResponse)
+				return &Backend{
+					client: remoteClient,
+					cache:  cacheable,
+				}
+			},
+			request: threescale.Request{
+				Auth: api.ClientAuth{
+					Type:  api.ProviderKey,
+					Value: "any",
+				},
+				Service: "test",
+				Transactions: []api.Transaction{
+					{
+						Metrics: api.Metrics{"hits": 2},
+						Params: api.Params{
+							AppID: "application",
+						},
+					},
+				},
+			},
+			expectResult: &threescale.AuthorizeResult{Authorized: false},
+		},
+		{
+			name: "Test failure on metrics with no parents that breach the limits - cache hit",
+			setup: func(cacheable Cacheable, remoteClient *mockRemoteClient) *Backend {
+
+				remoteResponse := &threescale.AuthorizeResult{
+					Authorized: true,
+					AuthorizeExtensions: threescale.AuthorizeExtensions{
+						Hierarchy:  make(api.Hierarchy),
+						RateLimits: nil,
+						UsageReports: api.UsageReports{
+							"hits": api.UsageReport{
+								PeriodWindow: api.PeriodWindow{
+									Period: api.Minute,
+								},
+								MaxValue:     4,
+								CurrentValue: 3,
+							},
+						},
+					},
+				}
+
+				fullRemoteState := getApplicationFromResponse(remoteResponse)
+
+				lc := make(LimitCounter)
+				lc["hits"] = make(map[api.Period]*Limit)
+				lc["hits"][api.Minute] = &Limit{
+					current: 3,
+					max:     4,
+				}
+
+				app := &Application{
+					RemoteState:      fullRemoteState.LimitCounter,
+					LimitCounter:     lc,
+					UnlimitedCounter: make(map[string]int),
+				}
+
+				cacheable.Set(cacheKey, app)
+
+				return &Backend{
+					client: remoteClient,
+					cache:  cacheable,
+				}
+			},
+			request: threescale.Request{
+				Auth: api.ClientAuth{
+					Type:  api.ProviderKey,
+					Value: "any",
+				},
+				Service: "test",
+				Transactions: []api.Transaction{
+					{
+						Metrics: api.Metrics{"hits": 2},
+						Params: api.Params{
+							AppID: "application",
+						},
+					},
+				},
+			},
+			expectResult: &threescale.AuthorizeResult{Authorized: false},
+		},
+		{
+			name: "Test failure when hierarchy should cause a breach of limits - cache miss",
+			setup: func(cacheable Cacheable, remoteClient *mockRemoteClient) *Backend {
+				// we want to ensure the conversion is done here and the cache is populated correctly
+				remoteResponse := &threescale.AuthorizeResult{
+					Authorized: true,
+					AuthorizeExtensions: threescale.AuthorizeExtensions{
+						Hierarchy:  api.Hierarchy{"hits": []string{"hits_child"}},
+						RateLimits: nil,
+						UsageReports: api.UsageReports{
+							"hits": api.UsageReport{
+								PeriodWindow: api.PeriodWindow{
+									Period: api.Minute,
+								},
+								MaxValue:     4,
+								CurrentValue: 3,
+							},
+						},
+					},
+				}
+
+				remoteClient.setAuthResponse(t, remoteResponse)
+				return &Backend{
+					client: remoteClient,
+					cache:  cacheable,
+				}
+			},
+			request: threescale.Request{
+				Auth: api.ClientAuth{
+					Type:  api.ProviderKey,
+					Value: "any",
+				},
+				Service: "test",
+				Transactions: []api.Transaction{
+					{
+						Metrics: api.Metrics{"hits_child": 2},
+						Params: api.Params{
+							AppID: "application",
+						},
+					},
+				},
+			},
+			expectResult: &threescale.AuthorizeResult{Authorized: false},
+		},
+		{
+			name: "Test success case cache miss",
+			setup: func(cacheable Cacheable, remoteClient *mockRemoteClient) *Backend {
+				// we want to ensure the conversion is done here and the cache is populated correctly
+				remoteResponse := &threescale.AuthorizeResult{
+					Authorized: true,
+					AuthorizeExtensions: threescale.AuthorizeExtensions{
+						Hierarchy:  api.Hierarchy{"hits": []string{"hits_child"}},
+						RateLimits: nil,
+						UsageReports: api.UsageReports{
+							"hits": api.UsageReport{
+								PeriodWindow: api.PeriodWindow{
+									Period: api.Minute,
+								},
+								MaxValue:     4,
+								CurrentValue: 3,
+							},
+						},
+					},
+				}
+
+				remoteClient.setAuthResponse(t, remoteResponse)
+				return &Backend{
+					client: remoteClient,
+					cache:  cacheable,
+				}
+			},
+			request: threescale.Request{
+				Auth: api.ClientAuth{
+					Type:  api.ProviderKey,
+					Value: "any",
+				},
+				Service: "test",
+				Transactions: []api.Transaction{
+					{
+						Metrics: api.Metrics{"orphan": 2, "hits": 1},
+						Params: api.Params{
+							AppID: "application",
+						},
+					},
+				},
+			},
+			expectResult: &threescale.AuthorizeResult{Authorized: true},
+			expectCacheState: Application{
+				auth: api.ClientAuth{
+					Type:  api.ProviderKey,
+					Value: "any",
+				},
+				params: api.Params{
+					AppID: "application",
+				},
+				RemoteState: newLimitCounter(t, "hits", api.Minute, &Limit{
+					current: 3,
+					max:     4,
+				}),
+				LimitCounter: newLimitCounter(t, "hits", api.Minute, &Limit{
+					current: 4,
+					max:     4,
+				}),
+				UnlimitedCounter: map[string]int{"orphan": 2},
+			},
+		},
+		{
+			name: "Test success case cache hit",
+			setup: func(cacheable Cacheable, remoteClient *mockRemoteClient) *Backend {
+				remoteResponse := &threescale.AuthorizeResult{
+					Authorized: true,
+					AuthorizeExtensions: threescale.AuthorizeExtensions{
+						Hierarchy:  api.Hierarchy{"hits": []string{"hits_child"}},
+						RateLimits: nil,
+						UsageReports: api.UsageReports{
+							"hits": api.UsageReport{
+								PeriodWindow: api.PeriodWindow{
+									Period: api.Minute,
+								},
+								MaxValue:     4,
+								CurrentValue: 3,
+							},
+						},
+					},
+				}
+
+				fullRemoteState := getApplicationFromResponse(remoteResponse)
+
+				lc := make(LimitCounter)
+				lc["hits"] = make(map[api.Period]*Limit)
+				lc["hits"][api.Minute] = &Limit{
+					current: 3,
+					max:     4,
+				}
+
+				app := &Application{
+					auth: api.ClientAuth{
+						Type:  api.ProviderKey,
+						Value: "any",
+					},
+					params: api.Params{
+						AppID: "application",
+					},
+					RemoteState:      fullRemoteState.RemoteState,
+					LimitCounter:     lc,
+					UnlimitedCounter: map[string]int{"orphan": 1},
+				}
+
+				cacheable.Set(cacheKey, app)
+
+				return &Backend{
+					client: remoteClient,
+					cache:  cacheable,
+				}
+			},
+			request: threescale.Request{
+				Auth: api.ClientAuth{
+					Type:  api.ProviderKey,
+					Value: "any",
+				},
+				Service: "test",
+				Transactions: []api.Transaction{
+					{
+						Metrics: api.Metrics{"orphan": 2, "hits": 1},
+						Params: api.Params{
+							UserKey: "application",
+						},
+					},
+				},
+			},
+			expectResult: &threescale.AuthorizeResult{Authorized: true},
+			expectCacheState: Application{
+				auth: api.ClientAuth{
+					Type:  api.ProviderKey,
+					Value: "any",
+				},
+				params: api.Params{
+					AppID: "application",
+				},
+				RemoteState: newLimitCounter(t, "hits", api.Minute, &Limit{
+					current: 3,
+					max:     4,
+				}),
+				LimitCounter: newLimitCounter(t, "hits", api.Minute, &Limit{
+					current: 4,
+					max:     4,
+				}),
+				UnlimitedCounter: map[string]int{"orphan": 3},
+			},
+		},
+	}
+
+	for _, input := range inputs {
+		t.Run(input.name, func(t *testing.T) {
+			remoteClient := &mockRemoteClient{}
+			b := input.setup(NewLocalCache(), remoteClient)
+
+			resp, err := b.AuthRep(input.request)
+			if err != nil {
+				if !input.expectError {
+					t.Errorf("unexpeced error - %s", err.Error())
+				}
+				return
+			}
+
+			equals(t, resp.Authorized, input.expectResult.Authorized)
+			equals(t, resp.Hierarchy, api.Hierarchy(nil))
+			equals(t, resp.UsageReports, api.UsageReports(nil))
+			equals(t, resp.RateLimits, (*api.RateLimits)(nil))
+
+			cachedVal, _ := b.cache.Get(cacheKey)
+
+			if resp.Authorized {
+				equals(t, input.expectCacheState.UnlimitedCounter, cachedVal.UnlimitedCounter)
+				equals(t, input.expectCacheState.LimitCounter, cachedVal.LimitCounter)
+				equals(t, input.expectCacheState.RemoteState, cachedVal.RemoteState)
+
+				equals(t, cachedVal.params, input.expectCacheState.params)
+				equals(t, cachedVal.auth, input.expectCacheState.auth)
+			}
+		})
+	}
+}
+
 func TestBackend_GetPeer(t *testing.T) {
 	mc := &mockRemoteClient{}
 	b := &Backend{
@@ -294,6 +676,14 @@ func equals(t *testing.T, exp, act interface{}) {
 		fmt.Printf("\033[31m%s:%d:\n\n\texp: %#v\n\n\tgot: %#v\033[39m\n\n", filepath.Base(file), line, exp, act)
 		t.Error("unexpected result when calling equals")
 	}
+}
+
+func newLimitCounter(t *testing.T, metric string, period api.Period, limit *Limit) LimitCounter {
+	t.Helper()
+	lc := make(LimitCounter)
+	lc[metric] = make(map[api.Period]*Limit)
+	lc[metric][period] = limit
+	return lc
 }
 
 // mocks and helpers *****************
