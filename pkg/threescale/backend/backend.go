72a45388deb6283272fcc8bbf83de69b709ec93c
Philip Gough <philip.p.gough@gmail.com>
2020-02-13 17:15:41 +0000

    backend: Add Flush functionality to cached backend
    The Flush API reports local state to the remote 3scale.
    After reporting, it attempts to get the updated state from 3scale for
    each application and modify the local state accordingly, depending on
    the outcome of both the report, and get updated state request over the
    network.
    
    It is expected that Flush will be run at a regular interval as a
    background process, however this is left to the caller and will not be
    included as part of this package.
    
    The Flush API takes a write lock so may temporarily block incoming
    requests to Auth,AuthRep and Report for the same application while doing so.

--- pkg/threescale/backend/backend.go
+++ pkg/threescale/backend/backend.go
@@ -229,6 +229,10 @@ func (b *Backend) remoteAuth(request threescale.Request) (*threescale.AuthorizeR
 	return b.client.Authorize(request)
 }
 
+func (b *Backend) remoteReport(request threescale.Request) (*threescale.ReportResult, error) {
+	return b.client.Report(request)
+}
+
 // localReport takes a write lock on the application and reports to the cache
 func (b *Backend) localReport(cacheKey string, metrics api.Metrics) {
 	// note - this should not miss since we will have returned an error prior to this if
@@ -253,9 +257,222 @@ func (b *Backend) localReport(cacheKey string, metrics api.Metrics) {
 	b.cache.Set(cacheKey, application)
 }
 
+// Flush the cached entries and report existing state to backend
+func (b *Backend) Flush() {
+	b.flush()
+}
+
+// cachedApp provides a placeholder for information for cache flushing
+type cachedApp struct {
+	// snapshot stores the app state (copy) as it was when we attempted to flush the cache
+	snapshot     Application
+	cacheKey     string
+	serviceID    api.Service
+	appID        string
+	reportingErr bool
+	authErr      bool
+	authResp     *threescale.AuthorizeResult
+	// deltas stores the metrics (copy) that were actually reported to 3scale after
+	// taking the hierarchy into account
+	deltas api.Metrics
+}
+
+func (b *Backend) flush() {
+	// create a hash of cached applications under a serviceApps ID
+	// this may not be required down in future and we can simplify but
+	// this gives us the flexibility to do batched reporting
+	groupedKeys := b.groupCachedItems()
+	for _, serviceApps := range groupedKeys {
+
+		// the first stage of flushing the cache involves calculating the value to be reported to 3scale
+		// and reporting remotely
+		b.handleFlushReporting(serviceApps)
+
+		// after we have reported for each known app under this service, we assume backend has finished its
+		// processing from the queue and authorize the app with a blank request to fetch updated state
+		// saving the response in the process
+		for i, cachedApp := range serviceApps {
+			req := getEmptyAuthRequest(cachedApp.serviceID, cachedApp.snapshot.auth, cachedApp.snapshot.params)
+			resp, err := b.remoteAuth(req)
+			if err != nil {
+				serviceApps[i].authErr = true
+			}
+			serviceApps[i].authResp = resp
+		}
+
+		// update the entry in the cache
+		b.handleFlushCacheUpdate(serviceApps)
+	}
+}
+
+func (b *Backend) groupCachedItems() map[string][]cachedApp {
+	keys := b.cache.Keys()
+	groupedServices := make(map[string][]cachedApp)
+
+	for _, key := range keys {
+		svc, app, err := parseCacheKey(key)
+		if err == nil {
+			groupedServices[svc] = append(groupedServices[svc], cachedApp{
+				cacheKey:  key,
+				serviceID: api.Service(svc),
+				appID:     app,
+			})
+		}
+	}
+	return groupedServices
+}
+
+// handleFlushReporting takes a read lock on the cached application and a snapshot of the app state and releases the
+// lock before computing the data which must be reported to 3scales backend
+func (b *Backend) handleFlushReporting(apps []cachedApp) {
+	for i, cachedApp := range apps {
+		app, ok := b.cache.Get(cachedApp.cacheKey)
+		if !ok {
+			continue
+		}
+		// for each application for this service
+		// take a read lock and take a snapshot of the current state
+		app.RLock()
+		// take a snapshot of the current application
+		apps[i].snapshot = app.deepCopy()
+		app.RUnlock()
+		appClone := &apps[i].snapshot
+
+		// calculate the deltas and report remotely
+		deltas := appClone.calculateDeltas()
+		// store the deltas for further use
+		apps[i].deltas = deltas
+		req := threescale.Request{
+			Auth:    appClone.auth,
+			Service: cachedApp.serviceID,
+			Transactions: []api.Transaction{
+				{
+					Metrics: deltas,
+					Params:  app.params,
+				},
+			},
+			Extensions: api.Extensions{
+				api.FlatUsageExtension: "1",
+			},
+		}
+		_, err := b.remoteReport(req)
+		if err != nil {
+			apps[i].reportingErr = true
+		}
+	}
+}
+
+// handleFlushCacheUpdate updates a cached entry (taking a write lock) using the prior flushing steps as a decision tree.
+// A description of the terminology used below is as follows:
+//     snapshot_hits - the value in local state when the snapshot is taken
+//     new_auth_hits - the current value for a metric as fetched from 3scale after successful authorization
+//     to_report_hits - the deltas that are calculated prior to reporting = current local state - last known 3scale state
+//     actually_reported_hits - is equal 0 (if report fails) || is equal to to_report_hits (if report succeeded)
+//     remote_state - local representation of state in remote 3scale
+// We need to handle the following cases ("hits" is used as an example metric name for simplification):
+// 1. reporting error and authorization error -> nothing to do, no change of state
+// 2,3. reporting error and authorization success, report success and authorization success -> hits += new_auth_hits - (snapshot_hits - to_report_hits ) - actually_reported_hits
+// 4. report success and authorization error -> remote_state = remote_state + actually_reported_hits. No change to local counters.
+// To summarise, if we have a reporting error, we will not amend our local representation of the remote state. if we have an authorization error we will not amend our local state.
+// In both of these cases, a successful call instead of an error will result in a change in the mentioned state
+func (b *Backend) handleFlushCacheUpdate(apps []cachedApp) {
+	for _, cachedApp := range apps {
+		// handle case (1)
+		if cachedApp.reportingErr && cachedApp.authErr {
+			// nothing to do here only continue on
+			// todo add logging
+			continue
+		}
+
+		//now that we have handled the dual error case, we know we need to
+		// read from the cache and modify the entry so we take a write lock
+		app, ok := b.cache.Get(cachedApp.cacheKey)
+		if !ok {
+			continue
+		}
+
+		// handle case (3)
+		if !cachedApp.reportingErr && cachedApp.authErr {
+			app.Lock()
+			// This is necessary when, during flushing, we have reported correctly but failed to authorize
+			// we dont have an updated state from 3scale so in order to avoid re-reporting what we have already
+			// successfully reported. We can achieve this safely by modifying our last know local
+			// representation of the remote state with the deltas we know that 3scale has processed.
+			app.addDeltasToRemoteState(cachedApp.deltas)
+			// we need to account for activity in between while adjusting our unlimited counter
+			app.pruneUnlimitedCounter(cachedApp.snapshot)
+			b.cache.Set(cachedApp.cacheKey, app)
+			app.Unlock()
+			continue
+		}
+
+		// handles cases (2, 4)
+		remoteState := getApplicationFromResponse(cachedApp.authResp).RemoteState
+
+		app.Lock()
+		app.adjustLocalState(cachedApp, remoteState)
+		b.cache.Set(cachedApp.cacheKey, app)
+		app.Unlock()
+	}
+}
+
 // GetPeer returns the hostname of the connected backend
 func (b *Backend) GetPeer() string {
 	return b.client.GetPeer()
+}
+
+// calculateDeltas returns a set of metrics that we can report to 3scale using the lowest period known in the counters
+// for metrics with rate limits attached
+func (a *Application) calculateDeltas() api.Metrics {
+	deltas := make(api.Metrics)
+
+	for metric := range a.LimitCounter {
+		lowestPeriod := a.LimitCounter.getLowestKnownPeriodForMetric(metric)
+		if lowestPeriod != nil {
+			if lastKnownState, ok := a.RemoteState[metric][*lowestPeriod]; ok {
+				toReport := a.LimitCounter[metric][*lowestPeriod].current - lastKnownState.current
+				deltas.Add(metric, toReport)
+			}
+		}
+	}
+
+	// now add the metrics with no limits to the delta
+	for unlimitedMetric, reportWithValue := range a.UnlimitedCounter {
+		deltas[unlimitedMetric] = reportWithValue
+	}
+	return deltas
+}
+
+// addDeltasToRemoteState modifies the remote state with the provided deltas
+func (a *Application) addDeltasToRemoteState(deltas api.Metrics) *Application {
+	for metric, value := range deltas {
+		periods, ok := a.RemoteState[metric]
+		if !ok {
+			continue
+		}
+
+		for period, _ := range periods {
+			a.RemoteState[metric][period].current += value
+		}
+	}
+
+	return a
+}
+
+// deepCopy creates a clone of the Application 'a'
+func (a *Application) deepCopy() Application {
+	unlimitedHitsClone := make(map[string]int, len(a.UnlimitedCounter))
+	for metric, val := range a.UnlimitedCounter {
+		unlimitedHitsClone[metric] = val
+	}
+
+	return Application{
+		RemoteState:      a.RemoteState,
+		LimitCounter:     a.LimitCounter.deepCopy(),
+		UnlimitedCounter: unlimitedHitsClone,
+		params:           a.params,
+		auth:             a.auth,
+	}
 }
 
 // updateUnlimitedCounter modifies the Applications 'UnlimitedCounter' field
@@ -269,6 +486,64 @@ func (a *Application) updateUnlimitedCounter(metric string, incrementBy int) {
 	a.UnlimitedCounter[metric] = current + incrementBy
 }
 
+// pruneUnlimitedCounter resets a metrics counter to the difference between the provided old value and the current value
+// If the end result is not at least 1, the metric is pruned from the counter
+func (a *Application) pruneUnlimitedCounter(snapshot Application) {
+	for metric, value := range a.UnlimitedCounter {
+		oldValue, ok := snapshot.UnlimitedCounter[metric]
+		if !ok {
+			delete(a.UnlimitedCounter, metric)
+		}
+
+		value -= oldValue
+		if value < 1 {
+			delete(a.UnlimitedCounter, metric)
+		}
+	}
+}
+
+// adjustLocalState assumes that we have a new remote state (set on a) fetched from 3scale and modifies local state based
+// on the state obtained during cache flushing
+func (a *Application) adjustLocalState(flushingState cachedApp, remoteState LimitCounter) *Application {
+	a.RemoteState = remoteState
+
+	for metric := range a.LimitCounter {
+		lowestPeriod := a.LimitCounter.getLowestKnownPeriodForMetric(metric)
+
+		if lowestPeriod == nil {
+			continue
+		}
+
+		snapped, ok := flushingState.snapshot.LimitCounter[metric][*lowestPeriod]
+		if !ok {
+			continue
+		}
+
+		latest, ok := a.RemoteState[metric][*lowestPeriod]
+		if !ok {
+			continue
+		}
+
+		lowestLimit := a.LimitCounter[metric][*lowestPeriod]
+
+		var reportedHits int
+		delta := flushingState.deltas[metric]
+		if !flushingState.reportingErr {
+			reportedHits = delta
+		}
+		updated := lowestLimit.current + latest.current - (snapped.current - delta) - reportedHits
+		lowestLimit.current = updated
+		lowestLimit.max = a.RemoteState[metric][*lowestPeriod].max
+	}
+
+	if !flushingState.reportingErr {
+		// reset the counter if we have reported already
+		a.pruneUnlimitedCounter(flushingState.snapshot)
+	}
+
+	return a
+}
+
 // deepCopy creates a clone of the LimitCounter 'lc'
 func (lc LimitCounter) deepCopy() LimitCounter {
 	clone := make(LimitCounter)
@@ -279,4 +554,23 @@ func (lc LimitCounter) deepCopy() LimitCounter {
 		}
 	}
 	return clone
+}
+
+var ascendingPeriodSequence = []api.Period{api.Minute, api.Hour, api.Day, api.Week, api.Month, api.Eternity}
+
+func (lc LimitCounter) getLowestKnownPeriodForMetric(metric string) *api.Period {
+	var period *api.Period
+
+	periodMap, ok := lc[metric]
+	if !ok {
+		return nil
+	}
+
+	for _, lowestKnownPeriod := range ascendingPeriodSequence {
+		if _, ok := periodMap[lowestKnownPeriod]; ok {
+			period = &lowestKnownPeriod
+			break
+		}
+	}
+	return period
 }
