fc003c2cabfcec6a48ab6ea0dd9860c3ac6021e1
Philip Gough <philip.p.gough@gmail.com>
2020-03-31 16:06:11 +0100

    backend: Add helper to check for deadlines exceeded

--- pkg/threescale/backend/helpers.go
+++ pkg/threescale/backend/helpers.go
@@ -194,6 +194,35 @@ func contains(key string, in []string) bool {
 	return false
 }
 
+// todo - this function is currently safe to use because we only provide it with timestamps and
+// deadlines that have been learned from 3scale backend. This means they are already correctly set
+// at the beginning of a specific period. That is to say, for example f(hour, 12:23:39) => 12:00:00 to 12:59:59
+// an hour has been passed through func f and its timestamp set to 12:00:00 and deadline to 12:59:59
+// This works because we only check deadlines on successful auth responses. *When* we provide adjustments for other
+// use cases, f(granularity api.Period, timestamp int64) will need to be created.
+func hasSurpassedDeadline(timestamp int64, granularity api.Period, deadline time.Time) bool {
+	switch granularity {
+	case api.Minute:
+		return time.Unix(timestamp, 0).Add(time.Minute).After(deadline)
+	case api.Hour:
+		return time.Unix(timestamp, 0).Add(time.Hour).After(deadline)
+	case api.Day:
+		return time.Unix(timestamp, 0).AddDate(0, 0, 1).After(deadline)
+	case api.Week:
+		// todo - needs revisiting as may not be technically correct with the way 3scale handles weeks
+		// time.Unix(timestamp, 0).AddDate(0, 0, 7).After(deadline)
+		return false
+	case api.Month:
+		return time.Unix(timestamp, 0).AddDate(0, 1, 0).After(deadline)
+	case api.Year:
+		return time.Unix(timestamp, 0).AddDate(1, 0, 0).After(deadline)
+	case api.Eternity:
+		return false
+	default:
+		return false
+	}
+}
+
 // newApplication creates a new, empty application with maps initialised
 func newApplication() *Application {
 	return &Application{
